34b99dbb088a309c8c25255ebf9d34d4f0aaacd9
Sultan Md. Ayman <32176118+sm-ayman@users.noreply.github.com>
2022-10-01 20:32:16 +0600

    Functionality Added

---
+++ app.js
@@ -0,0 +1,31 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+console.log(buttons);
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break; 
+            
+            case '=': 
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch{
+                    display.innerText = 'OPS! An Error Occured';
+                }
+                break;
+
+            default:
+                display.innerText += e.target.innerText;
+        }
+    })
+})
